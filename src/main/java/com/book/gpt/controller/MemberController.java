e98963c4d729dda541721104bc9f2240a9dc2bb2
jojo6807 <jyj05181@gmail.com>
2023-11-06 10:10:38 +0900

    조영준: 회원 정보 관리

--- src/main/java/com/book/gpt/controller/MemberController.java
+++ src/main/java/com/book/gpt/controller/MemberController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.crypto.SecretKey;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -91,6 +92,63 @@ public class MemberController {
         } catch (Exception e) {
             return new ResponseEntity<>("Error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    // 조영준
+    // 회원 정보 조회
+    @GetMapping("/member")
+    public ResponseEntity<List<MemberDTO>> memberList(@RequestParam String id) {
+        System.out.println("id : " + id);
+        MemberDAO dao = new MemberDAO();
+        List<MemberDTO> list = dao.memberInfo(id);
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+    // 입력 받은 이름, 아이디, 비밀번호, 이메일로 정보 조회
+    @GetMapping("/checkInfo")
+    public ResponseEntity<Boolean> memberCheck(@RequestParam String name,@RequestParam String id,@RequestParam String pw,@RequestParam String email) {
+        MemberDAO dao = new MemberDAO();
+        boolean isTrue = dao.memberCheck(name, id, pw, email);
+        return new ResponseEntity<>(isTrue, HttpStatus.OK);
+    }
+    // 아이디 변경
+    @PostMapping("/updateId")
+    public ResponseEntity<Boolean> updateId (@RequestBody Map<String, String> updateId) {
+        String currentId = updateId.get("currentId");
+        String newId = updateId.get("newId");
+        MemberDAO dao = new MemberDAO();
+        boolean isTrue = dao.modifyId(currentId, newId);
+        System.out.println(isTrue);
+        return new ResponseEntity<>(isTrue, HttpStatus.OK);
+    }
+    // 비밀번호 변경
+    @PostMapping("/updatePw")
+    public ResponseEntity<Boolean> updatePw (@RequestBody Map<String, String> updatePw) {
+        // 프론트엔드의 aixos의 키값과 동일한 이름일 것
+        String currentPw = updatePw.get("currentPw");
+        String newPw = updatePw.get("newPw");
+        MemberDAO dao = new MemberDAO();
+        boolean isTrue = dao.modifyPw(currentPw, newPw);
+        System.out.println(isTrue);
+        return new ResponseEntity<>(isTrue, HttpStatus.OK);
+    }
+    // 정보 삭제, 회원탈퇴
+    @PostMapping("/delete")
+    public ResponseEntity<Boolean> deleteMember (@RequestBody Map<String, String> del) {
+        String getId = del.get("delId");
+        MemberDAO dao = new MemberDAO();
+        boolean isTrue = dao.deleteMember(getId);
+        System.out.println("삭제여부 : " + isTrue);
+        return new ResponseEntity<>(isTrue, HttpStatus.OK);
+    }
+    // 금액 충전
+    @PostMapping("/charge")
+    public ResponseEntity<Boolean> chargeCash (@RequestBody Map<String, Object> charge) {
+        String getId = (String) charge.get("id");
+        int getCash = (int) charge.get("cash");
+        MemberDAO dao = new MemberDAO();
+        boolean isTrue = dao.chargingCash(getId, getCash);
+        System.out.println("충전 여부" + isTrue);
+        return new ResponseEntity<>(isTrue, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/book/gpt/dto/MemberDTO.java
+++ src/main/java/com/book/gpt/dto/MemberDTO.java
@@ -18,4 +18,19 @@ public class MemberDTO {
     private boolean isAdmin;
     private Collection<? extends GrantedAuthority> authorities; // 권한 정보
 
+
+    // 조영준
+    // @setter가 안먹어서 일단 만듦.
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+    public void setCash(int cash) {
+        this.cash = cash;
+    }
 }
