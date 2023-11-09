d58c378e7439b3307c8dd5206dc4463d5208909e
JH <dafr47@naver.com>
2023-11-09 11:05:06 +0900

    장바구니 추가

--- src/main/java/com/book/gpt/dao/BookDAO.java
+++ src/main/java/com/book/gpt/dao/BookDAO.java
@@ -6,8 +6,11 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -80,5 +83,28 @@ public class BookDAO {
                 bookDTO.getPrice(), bookDTO.getPublishYear(), id);
         return findBook(id);
     }
+    // 길종환
+    public BookDTO getBookInfo(int bookId) {
+        String sql = "SELECT * FROM Book WHERE ID = ?";
 
+        return jdbcTemplate.queryForObject(sql, new RowMapper<BookDTO>() {
+            @Override
+            public BookDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new BookDTO(
+                        rs.getInt("ID"),
+                        rs.getString("TITLE"),
+                        rs.getString("AUTHOR"),
+                        rs.getString("PUBLISHER"),
+                        rs.getString("GENRE"),
+                        rs.getString("IMAGE_URL"),
+                        rs.getString("CONTENT_URL"),
+                        rs.getString("SUMMARY"),
+                        rs.getInt("PRICE"),
+                        rs.getDate("PUBLISH_YEAR"),
+                        rs.getDate("ENTRY_TIME"),
+                        rs.getInt("PURCHASE_COUNT")
+                );
+            }
+        }, bookId);
+    }
 }
